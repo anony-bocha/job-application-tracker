1b9adfb2cefae8fd84d3cd1264b7096fcedf8578
yonatan semate <semateyonatan@gmail.com>
2025-07-18 14:32:20 +0300

    Add email notifications on new job application using signals

--- jobtracker/applications/apps.py
+++ jobtracker/applications/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class ApplicationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications'
+
+    def ready(self):
+        import applications.signals
